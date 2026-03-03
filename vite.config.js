04baf9419966ba52ca9527d1413ec48fa4a78051
Abhishekydv25 <yaduvanshi1425@gmail.com>
2026-03-03 19:38:22 +0530

    fix base path and vercel config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/holi-ujala/",
-  build: {
-    chunkSizeWarningLimit: 5000, // base64 images are large
-  },
+  base: "./",
 });
