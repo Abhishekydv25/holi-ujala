b8dbf403723ae0be9a069fcd017593b5c430fab5
Abhishekydv25 <yaduvanshi1425@gmail.com>
2026-03-03 18:45:21 +0530

    added base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/holi-ujala/",
   build: {
     chunkSizeWarningLimit: 5000, // base64 images are large
   },
